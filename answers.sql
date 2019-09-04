f8cb040956fbaf1b317f447c2468b5586e67d371
Tramy Nguyen <tramynn2@gmail.com>
2019-09-04 15:43:47 -0500

    completed table employee

--- answers.sql
+++ answers.sql
@@ -86,3 +86,16 @@ SELECT * FROM artist WHERE name LIKE 'Black%';
 -- 5) Select all artists that contain the word 'Black'.
 SELECT * FROM artist WHERE name = '%Black%';
 
+-- TABLE - employee
+-- 1) List all employee first and last names only that live in Calgary.
+SELECT first_name, last_name FROM employee WHERE city = ('Calgary');
+-- 2) Find the birthdate for the youngest employee.
+SELECT MAX(birth_date) FROM employee;
+-- 3) Find the birthdate for the oldest employee.
+SELECT MIN(birth_date) FROM employee;
+-- 4) Find everyone that reports to Nancy Edwards (Use the ReportsTo column).
+SELECT * FROM employee where reports_to = 2;
+-- 5) You will need to query the employee table to find the Id for Nancy Edwards
+SELECT * FROM employee WHERE reports_to = 2;
+-- 6) Count how many people live in Lethbridge.
+SELECT COUNT(*) FROM employee WHERE city = 'Lethbridge';
