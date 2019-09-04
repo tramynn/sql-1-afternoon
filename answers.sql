1a5fb79e26ed5c1e85e8309ae7a07df7180ce9ee
Tramy Nguyen <tramynn2@gmail.com>
2019-09-04 14:44:42 -0500

    completed table - artist

--- answers.sql
+++ answers.sql
@@ -71,4 +71,18 @@ SELECT SUM(product_price) FROM orders;
 SELECT SUM(product_price * quantity) FROM orders WHERE person_id = 1;
 
 -- TABLE -- ARTIST
+-- 1) Add 3 new artists to the artist table. ( It's already created )
+INSERT INTO artist (name)
+VALUES 
+('Marshmello'),
+('EDX'),
+('AC Slater');
+-- 2) Select 10 artists in reverse alphabetical order.
+SELECT * FROM artist ORDER BY name DESC LIMIT 10;
+-- 3) Select 5 artists in alphabetical order.
+SELECT * FROM artist ORDER BY name ASC LIMIT 5;
+-- 4) Select all artists that start with the word 'Black'.
+SELECT * FROM artist WHERE name LIKE 'Black%';
+-- 5) Select all artists that contain the word 'Black'.
+SELECT * FROM artist WHERE name = '%Black%';
 
